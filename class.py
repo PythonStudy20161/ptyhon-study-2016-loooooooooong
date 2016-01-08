4d4ccccdeb178a809df017237efb65445ee058c7
choy Wang <wangcy1202@qq.com>
2016-01-08 14:52:08 +0800

    Create class.py

---
+++ class.py
@@ -0,0 +1,19 @@
+#_*_coding:utf-8_*_
+#类定义
+class people: 
+#定义基本属性 
+    name = '' 
+    age = 0 
+#定义私有属性,私有属性在类外部无法直接进行访问 
+    __weight = 0 
+#定义构造方法 
+    def __init__(self,n,a,w): 
+        self.name = n 
+        self.age = a 
+        self.__weight = w 
+    def speak(self): 
+        print("%s is speaking: I am %d years old:%d" %(self.name,self.age,self.__weight)) 
+
+     
+p = people('tom',10,30) 
+p.speak()
