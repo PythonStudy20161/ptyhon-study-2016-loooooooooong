08998d47c3faa4269d13c5716366ec43725e4d2c
choy Wang <wangcy1202@qq.com>
2016-01-08 16:01:17 +0800

    Update class.py

--- class.py
+++ class.py
@@ -1,12 +1,10 @@
 #_*_coding:utf-8_*_
 #类定义
+
 class people: 
-#定义基本属性 
     name = '' 
     age = 0 
-#定义私有属性,私有属性在类外部无法直接进行访问 
     __weight = 0 
-#定义构造方法 
     def __init__(self,n,a,w): 
         self.name = n 
         self.age = a 
@@ -14,6 +12,40 @@ class people:
     def speak(self): 
         print("%s is speaking: I am %d years old:%d" %(self.name,self.age,self.__weight)) 
 
-     
+#工程师继承人
+class engerneer(people):
+    C_language=''
+    def __init__(self,n,a,w,c):
+        people.__init__(self,n,a,w)
+        self.C_language=c
+    def speak(self): 
+        print("%s is speaking: I am %d years old,and I am use C program language %d"%(self.name,self.age,self.C_language))
+
+#默认不继承的管理职位
+class manager():
+    manager_year=''
+    def __init__(self,mey):
+        self.manager_year=mey
+    def speak(self):
+        print("%s is speaking: I am manager for %d years. "%(self.name,self.manager_year))
+
+#多重继承职位
+class VP(manager,engerneer):
+    count=''
+    def __init__(self,n,a,w,c,mey,count):
+        engerneer.__init__(self,n,a,w,c)
+        manager.__init__(self,mey)
+        self.count=count
+    def speak(self):
+        print("%s is speaking: I am %d years old,use C %d year,manager %d people."%(self.name,self.age,self.C_language,self.manager_year))
+
+
+
 p = people('tom',10,30) 
 p.speak()
+print "\n**************\n"
+e=engerneer('gerry',22,100,2)
+e.speak()
+print "\n**************\n"
+v=VP('boss',30,80,10,100,5)
+v.speak()
